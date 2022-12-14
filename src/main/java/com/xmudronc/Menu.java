2081f1473ac213a13662b71436c59edb4bfb7a51
Mudroncek, Ivan <imudroncek@netsuite.com>
2022-12-14 15:47:22 +0100

    Introduced universal areas.

--- src/main/java/com/xmudronc/Menu.java
+++ src/main/java/com/xmudronc/Menu.java
@@ -13,8 +13,6 @@ public class Menu {
     private Size runSize = new Size(120, 40);
     private Terminal terminal;
     private NonBlockingReader reader;
-    private Integer width = 20;
-    private Integer height = 20;
     private Integer buttonWidth = 20;
     private Option selectedOption;
     private Boolean running = false;
@@ -76,8 +74,8 @@ public class Menu {
     }
 
     public void clearGameArea() {
-        for (Integer y = 2; y < height; y++) {
-            for (Integer x = 3; x < (width*2)-2; x++) {
+        for (Integer y = 1; y < runSize.getRows(); y++) {
+            for (Integer x = 1; x < runSize.getRows()*2+4; x+=2) {
                 System.out.print(String.format("%c[%d;%df", 0x1B, y, x));
                 System.out.print("\u001B[0m" + Symbol.EMPTY.value);
             }
@@ -86,13 +84,13 @@ public class Menu {
     
     public void snajkSelected() throws IOException {
         clearGameArea();
-        Snajk snajk = new Snajk(this.terminal, this.reader, this.logArea, this.startupSize, this.width, this.height);
+        Snajk snajk = new Snajk(this.terminal, this.reader, this.logArea, this.startupSize, runSize.getRows(), runSize.getRows());
         snajk.init();
     }
 
     public void scoreSelected() throws IOException {
         clearGameArea();
-        HighScore highScore = new HighScore(this.terminal, this.reader, this.logArea, this.startupSize, this.width, this.height);
+        HighScore highScore = new HighScore(this.terminal, this.reader, this.logArea, this.startupSize, runSize.getRows(), runSize.getRows());
         highScore.init();
     }
 
@@ -104,37 +102,32 @@ public class Menu {
         System.exit(0);
     }
 
-    private void drawLogArea() {
-        Integer w = terminal.getWidth();
-        for (Integer y = 11; y < height+1; y++) {
-            for (Integer x = (width*2)+3; x < w-2; x++) {
+    private void drawArea(Integer fromY, Integer toY, Integer fromX, Integer toX) {
+        for (Integer y = fromY; y < toY; y++) {
+            for (Integer x = fromX; x < toX; x+=2) {
                 System.out.print(String.format("%c[%d;%df", 0x1B, y, x));
-                if (y == 11 || y == height) {
-                    if (x <= (width*2)+4) {
-                        System.out.print("\u001B[0m" + Symbol.EMPTY.value);
-                    } else {
-                        System.out.print("\u001B[41m" + Symbol.EMPTY.value);
-                    }
+                if (y == fromY || y == toY-1) {
+                    System.out.print("\u001B[41m" + Symbol.EMPTY.value);
                 } else {
-                    if (x == (width*2)+5 || x == (width*2)+6 || x == w-2 || x == w-3) {
+                    if (x == fromX || x == toX-1) {
                         System.out.print("\u001B[41m" + Symbol.EMPTY.value);
                     } else {
                         System.out.print("\u001B[0m" + Symbol.EMPTY.value);
                     }
                 }
             }
-            if (y != 11) {
-                System.out.print(String.format("%c[%d;%df", 0x1B, y, w-1));
+            if (y != fromY) {
+                System.out.print(String.format("%c[%d;%df", 0x1B, y, toX+1));
                 System.out.print("\u001B[30m\u001B[40m" + Symbol.BLOCK.value);
             } else {
-                System.out.print(String.format("%c[%d;%df", 0x1B, y, w-1));
+                System.out.print(String.format("%c[%d;%df", 0x1B, y, toX+1));
                 System.out.print("\u001B[0m" + Symbol.EMPTY.value);
             }
             System.out.println();
         }    
-        for (Integer x = (width*2)+3; x < w; x++) {
-            System.out.print(String.format("%c[%d;%df", 0x1B, height+1, x));
-            if (x <= (width*2)+6) {
+        for (Integer x = fromX; x < toX+2; x+=2) {
+            System.out.print(String.format("%c[%d;%df", 0x1B, toY, x));
+            if (x < fromX+2) {
                 System.out.print("\u001B[0m" + Symbol.EMPTY.value);
             } else {
                 System.out.print("\u001B[30m\u001B[40m" + Symbol.BLOCK.value);
@@ -143,48 +136,34 @@ public class Menu {
         System.out.print("\u001B[0m"); 
     }
 
+    private void drawLogArea() {
+        Integer fromY = 11;
+        Integer toY = runSize.getRows();
+        Integer fromX = runSize.getRows()*2+5;
+        Integer toX = runSize.getColumns()-2;
+        drawArea(fromY, toY, fromX, toX);
+    }
+
     public void drawScoreArea() {
-        Integer w = terminal.getWidth();
-        for (Integer y = 1; y < 9; y++) {
-            for (Integer x = (width*2)+3; x < w-2; x++) {
-                System.out.print(String.format("%c[%d;%df", 0x1B, y, x));
-                if (y == 1 || y == 8) {
-                    if (x <= (width*2)+4) {
-                        System.out.print("\u001B[0m" + Symbol.EMPTY.value);
-                    } else {
-                        System.out.print("\u001B[41m" + Symbol.EMPTY.value);
-                    }
-                } else {
-                    if (x == (width*2)+5 || x == (width*2)+6 || x == w-2 || x == w-3) {
-                        System.out.print("\u001B[41m" + Symbol.EMPTY.value);
-                    } else {
-                        System.out.print("\u001B[0m" + Symbol.EMPTY.value);
-                    }
-                }
-            }
-            if (y != 1) {
-                System.out.print(String.format("%c[%d;%df", 0x1B, y, w-1));
-                System.out.print("\u001B[30m\u001B[40m" + Symbol.BLOCK.value);
-            } else {
-                System.out.print(String.format("%c[%d;%df", 0x1B, y, w-1));
-                System.out.print("\u001B[0m" + Symbol.EMPTY.value);
-            }
-            System.out.println();
-        }    
-        for (Integer x = (width*2)+3; x < w; x++) {
-            System.out.print(String.format("%c[%d;%df", 0x1B, 9, x));
-            if (x <= (width*2)+6) {
-                System.out.print("\u001B[0m" + Symbol.EMPTY.value);
-            } else {
-                System.out.print("\u001B[30m\u001B[40m" + Symbol.BLOCK.value);
-            }
-        } 
-        System.out.print("\u001B[0m"); 
-        System.out.print(String.format("%c[%d;%df", 0x1B, 3, (width*2)+9));
+        Integer fromY = 1;
+        Integer toY = 9;
+        Integer fromX = runSize.getRows()*2+5;
+        Integer toX = runSize.getColumns()-2;
+        drawArea(fromY, toY, fromX, toX);
+
+        System.out.print(String.format("%c[%d;%df", 0x1B, 3, fromX+4));
         System.out.print("\u001B[37m" + "SCORE:");
-        System.out.print(String.format("%c[%d;%df", 0x1B, 6, (width*2)+9));
+        System.out.print(String.format("%c[%d;%df", 0x1B, 6, fromX+4));
         System.out.print("\u001B[37m" + "MULTIPLIER: ");
         System.out.print("\u001B[0m"); 
+    }
+
+    public void drawGameArea() {
+        Integer fromY = 1;
+        Integer toY = runSize.getRows();
+        Integer fromX = 1;
+        Integer toX = runSize.getRows()*2;
+        drawArea(fromY, toY, fromX, toX);
     }
 
     public void init() throws IOException {
@@ -193,14 +172,12 @@ public class Menu {
         startupSize = terminal.getSize();
         resizeTerminal(runSize.getColumns(), runSize.getRows());
         reader = terminal.reader();
-        clearGameArea();
-        drawBorder();
-        drawMenu();
-        logArea = new LogArea(87, 13, 28, 25);
+        logArea = new LogArea(89, 13, 26, 25);
+        init(terminal, reader, logArea, startupSize);
+        drawScoreArea();
         drawLogArea();
         logArea.printToLog("UP:     W\nDOWN:   S\nLEFT:   A\nRIGHT:  D\n\nSELECT: RETURN\nQUIT:   P\n\n");
-        logArea.printToLog("----------------------------\n");
-        drawScoreArea();
+        logArea.printToLog("--------------------------\n");
     }
 
     public void init(Terminal terminal, NonBlockingReader reader, LogArea logArea, Size startupSize) throws IOException {
@@ -209,45 +186,12 @@ public class Menu {
         this.logArea = logArea;
         this.startupSize = startupSize;
         clearGameArea();
-        drawBorder();
+        drawGameArea();
         drawMenu();
     }
 
     private void resizeTerminal(Integer columns, Integer rows) {
         System.out.print("\u001B[8;" + rows + ";" + columns + "t"); 
-    }
-
-    public void drawBorder() throws IOException {
-        Integer w = terminal.getWidth();
-        Integer h = terminal.getHeight();
-        Integer res = w<h?w:h;
-        width = res - 1;
-        height = res - 1;
-        System.out.print(String.format("%c[%d;%df", 0x1B, 1, 1));
-        for (Integer y = 0; y < height; y++) {
-            for (Integer x = 0; x < width; x++) {
-                if (y == 0 || y == height-1) {
-                    System.out.print("\u001B[41m" + Symbol.EMPTY.value);
-                } else {
-                    if (x == 0 || x == width-1) {
-                        System.out.print("\u001B[41m" + Symbol.EMPTY.value);
-                    } else {
-                        System.out.print("\u001B[0m" + Symbol.EMPTY.value);
-                    }
-                }
-            }
-            if (y != 0) {
-                System.out.print("\u001B[30m\u001B[40m" + Symbol.BLOCK.value);
-            } else {
-                System.out.print("\u001B[0m" + Symbol.EMPTY.value);
-            }
-            System.out.println();
-        }    
-        for (Integer x = 3; x < (width*2)+3; x+=2) {
-            System.out.print(String.format("%c[%d;%df", 0x1B, height+1, x));
-            System.out.print("\u001B[30m\u001B[40m" + Symbol.BLOCK.value);
-        } 
-        System.out.print("\u001B[0m");  
     }
 
     public void drawMenu() {
@@ -257,8 +201,8 @@ public class Menu {
     }
 
     public void prepareMenuOptions() {
-        int x = width;
-        int y = (height/2)-5;
+        int x = runSize.getRows();
+        int y = (runSize.getRows()/2)-5;
         Option newGame = new Option(OptionId.NEW_GAME, new Point(x, y), "New Game");
         Option highScore = new Option(OptionId.HIGH_SCORE ,new Point(x, y+5), "High Score");
         Option exit = new Option(OptionId.EXIT, new Point(x, y+10), "Exit");
